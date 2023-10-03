be71502bbd7880f1d92efcb518ea6fa8057eb878
Sergio Ruiz <sergiotto@outlook.com>
2023-10-03 11:31:12 +0200

    feat: add get id on edit-expense

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { HomeComponent } from './pages/home/home.component';
 import { IncomesComponent } from './pages/incomes/incomes.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
 import { OptionsComponent } from './pages/options/options.component';
-
+import { EditExpenseComponent } from './pages/edit-expense/edit-expense.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'incomes', component: IncomesComponent},
+  { path: 'incomes', component: IncomesComponent },
   { path: 'expenses', component: ExpensesComponent },
   { path: 'options', component: OptionsComponent },
+  { path: 'edit-expense/:id', component: EditExpenseComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/edit-expense/edit-expense.component.ts
+++ src/app/pages/edit-expense/edit-expense.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-edit-expense',
   templateUrl: './edit-expense.component.html',
-  styleUrls: ['./edit-expense.component.css']
+  styleUrls: ['./edit-expense.component.css'],
 })
 export class EditExpenseComponent {
+  id: any = '';
 
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this._route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id');
+    });
+  }
 }
